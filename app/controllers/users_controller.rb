3e8d4b592c0ec39bdf0a40c115516dddf8e9b66e
HakuryuSato <r700540@gmail.com>
2025-03-18 18:09:54 +0900

    strong parameterをexpectに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -66,6 +67,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.expect(user: [:name, :email, :password, :password_confirmation])
     end
 end
