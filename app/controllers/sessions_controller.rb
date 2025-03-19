0e10b2923bc94290f874180191709a344e8a36c7
HakuryuSato <r700540@gmail.com>
2025-03-19 19:40:07 +0900

    リダイレクトとエラー処理の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
   def create
-    render "new", status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      redirect_to login_path
+    else
+      # エラーメッセージを作成する
+      flash.now[:alert] = "エラーです"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
