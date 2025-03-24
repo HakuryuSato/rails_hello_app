b620cdae4f170af575b4494b4110889ee57b3559
HakuryuSato <r700540@gmail.com>
2025-03-24 19:51:34 +0900

    クラスメソッドの記述方法を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,34 @@
 class User < ApplicationRecord
-    has_many :microposts
-    before_save { self.email = email.downcase }
-    validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: true
-    has_secure_password validations: true
+  attr_accessor :remember_token
+  has_many :microposts
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  has_secure_password validations: true
 
-    # Skip password validation if password_digest is already set (for fixtures)
-    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  # Skip password validation if password_digest is already set (for fixtures)
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  # クラスメソッド
+  class << self
+    # 渡された文字列のハッシュ値を返す
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
-  # ランダムなトークンを返す
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  # 永続セッションのためのユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
