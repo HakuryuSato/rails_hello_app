416460e8dee52521e8413cd35a9d053adec1192f
HakuryuSato <r700540@gmail.com>
2025-03-24 21:42:43 +0900

    整形

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,17 @@ class SessionsController < ApplicationController
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       reset_session      # ログインの直前に必ずこれを書くこと
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      remember user
       log_in user
       redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = "Invalid email/password combination"
       render "new", status: :unprocessable_entity
+
     end
   end
+
+
 
   def destroy
     log_out if logged_in?
