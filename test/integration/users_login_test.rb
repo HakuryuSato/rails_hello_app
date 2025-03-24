c0d26dfa5af580e42073380c5d5eb5088649fb8c
HakuryuSato <r700540@gmail.com>
2025-03-24 21:37:41 +0900

    ログインテスト追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,6 +29,19 @@ class ValidLogin < UsersLogin
     post login_path, params: { session: { email:    @user.email,
                                           password: "password" } }
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: "1")
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: "0")
+    assert cookies[:remember_token].blank?
+  end
 end
 
 class ValidLoginTest < ValidLogin
