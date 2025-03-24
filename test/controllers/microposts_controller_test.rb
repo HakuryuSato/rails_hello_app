d0941cbba06afc6e86e605b7d1d6d4cc062ba835
HakuryuSato <r700540@gmail.com>
2025-03-24 17:25:16 +0900

    不要なテストコメントアウト

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,49 +1,49 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @micropost = microposts(:one)
-  end
+  # setup do
+  #   @micropost = microposts(:one)
+  # end
 
-  test "should get index" do
-    get microposts_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get microposts_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get new_micropost_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_micropost_url
+  #   assert_response :success
+  # end
 
-  test "should create micropost" do
-    user = users(:one)  # 既存のユーザーを取得
-    assert_difference("Micropost.count") do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: user.id } }
-    end
+  # test "should create micropost" do
+  #   user = users(:one)  # 既存のユーザーを取得
+  #   assert_difference("Micropost.count") do
+  #     post microposts_url, params: { micropost: { content: @micropost.content, user_id: user.id } }
+  #   end
 
-    assert_redirected_to micropost_url(Micropost.last)
-  end
+  #   assert_redirected_to micropost_url(Micropost.last)
+  # end
 
-  test "should show micropost" do
-    get micropost_url(@micropost)
-    assert_response :success
-  end
+  # test "should show micropost" do
+  #   get micropost_url(@micropost)
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get edit_micropost_url(@micropost)
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get edit_micropost_url(@micropost)
+  #   assert_response :success
+  # end
 
-  test "should update micropost" do
-    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
-    assert_redirected_to micropost_url(@micropost)
-  end
+  # test "should update micropost" do
+  #   patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+  #   assert_redirected_to micropost_url(@micropost)
+  # end
 
-  test "should destroy micropost" do
-    assert_difference("Micropost.count", -1) do
-      delete micropost_url(@micropost)
-    end
+  # test "should destroy micropost" do
+  #   assert_difference("Micropost.count", -1) do
+  #     delete micropost_url(@micropost)
+  #   end
 
-    assert_redirected_to microposts_url
-  end
+  #   assert_redirected_to microposts_url
+  # end
 end
