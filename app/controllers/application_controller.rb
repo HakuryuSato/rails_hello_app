a4cd6eb4e56ba79aa5805f647e7737cb7a5b9d8d
HakuryuSato <r700540@gmail.com>
2025-03-24 14:44:57 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def hello 
-    render html:"hola, mundo!"
+  def hello
+    render html: "hola, mundo!"
   end
 
   def goodbye
-    render html:"goodbye, world!"
+    render html: "goodbye, world!"
   end
 end
