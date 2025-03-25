5dbf0ff329a8bec8dbe3367c4369828936eb68b3
HakuryuSato <r700540@gmail.com>
2025-03-25 17:54:27 +0900

    ユーザーコントローラーに管理者ユーザー確認メソッドを追加し、管理者でない場合はリダイレクト処理を実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,4 +71,9 @@ class UsersController < ApplicationController
       redirect_to(root_url, status: :see_other)
     end
   end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
 end
