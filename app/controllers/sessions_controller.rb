f7a366ff01792dfeeae907dd198b093b7f3eb823
HakuryuSato <r700540@gmail.com>
2025-03-25 15:15:25 +0900

    整形

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,31 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      reset_session      # ログインの直前に必ずこれを書くこと
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+
+      reset_session
+
       log_in user
+
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
       redirect_to user
+
     else
-      # エラーメッセージを作成する
-      flash.now[:danger] = "Invalid email/password combination"
-      render "new", status: :unprocessable_entity
+
+      flash.now[:danger] = 'Invalid email/password combination'
+
+      render 'new', status: :unprocessable_entity
 
     end
   end
 
   def destroy
     log_out if logged_in?
+
     redirect_to root_url, status: :see_other
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,89 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
+
   def index
     @users = User.all
   end
 
   # GET /users/1 or /users/1.json
+
   def show
     @user = User.find(params[:id])
   end
 
   # GET /users/new
+
   def new
     @user = User.new
   end
 
   # GET /users/1/edit
+
   def edit
     @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render "new", status: :unprocessable_entity
-    end
-  end
 
-  def edit
-    @user = User.find(params[:id])
+    if @user.save
+
+      reset_session
+
+      log_in @user
+
+      flash[:success] = 'Welcome to the Sample App!'
+
+      redirect_to @user
+
+    else
+
+      render 'new', status: :unprocessable_entity
+
+    end
   end
 
   def update
     @user = User.find(params[:id])
+
     if @user.update(user_params)
-      # 更新に成功した場合を扱う
+
+      flash[:success] = 'Profile updated'
+
+      redirect_to @user
+
     else
-      render "edit", status: :unprocessable_entity
+
+      render 'edit', status: :unprocessable_entity
+
     end
   end
 
   # DELETE /users/1 or /users/1.json
+
   def destroy
     @user.destroy!
 
     respond_to do |format|
-      format.html { redirect_to users_path, status: :see_other, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_path, status: :see_other, notice: 'User was successfully destroyed.' }
+
       format.json { head :no_content }
     end
   end
 
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params.require(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.expect(user: [ :name, :email, :password, :password_confirmation ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+
+  def set_user
+    @user = User.find(params.require(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+
+  def user_params
+    params.expect(user: %i[name email password password_confirmation])
+  end
 end
