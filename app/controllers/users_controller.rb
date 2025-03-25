9c0284781902e2e6949e9e97a05065436a36da59
HakuryuSato <r700540@gmail.com>
2025-03-25 19:19:13 +0900

    ユーザーコントローラーのbefore_actionの書き方を統一し、テストにユーザー削除時のリダイレクト処理を追加。ユーザーフィクスチャに管理者権限を付与。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [ :index, :edit, :update ]
   before_action :set_user, only: %i[show edit update destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [ :edit, :update ]
   before_action :admin_user, only: :destroy
 
   def index
